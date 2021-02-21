c2e85895a90c99d0cd21f80a7b55cc76cedcfc2d
Rodolfo <rodolfomolinasv@gmail.com>
2021-02-21 11:52:09 -0600

    Add Products model

---
+++ models/Products.js
@@ -0,0 +1,19 @@
+const products = [
+    {
+        id: 1,
+        title: 'Product 1',
+        price: 10,
+    },
+    {
+        id: 2,
+        title: 'Product 2',
+        price: 20,
+    },
+    {
+        id: 3,
+        title: 'Product 3',
+        price: 30,
+    },
+];
+
+module.exports = products;
