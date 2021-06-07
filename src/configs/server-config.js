560dab9e3785fbc75411ad2da829ce6bf0b290a6
Rodolfo <rodolfomolinasv@gmail.com>
2021-06-07 13:32:39 -0600

    Add production server config

--- src/configs/server-config.js
+++ src/configs/server-config.js
@@ -1,6 +1,17 @@
-module.exports = {
-    scheme: 'http',
-    server: 'localhost',
-    ip: '127.0.0.1',
-    port: process.env.PORT || 5000,
-};
+if (process.env.NODE_ENV == 'production') {
+    module.exports = {
+        scheme: 'https',
+        server: 'online-store-312700.wn.r.appspot.com',
+        ip: process.env.IP,
+        port: process.env.PORT || 5000,
+        imagePort: '',
+    };
+} else {
+    module.exports = {
+        scheme: 'http',
+        server: 'localhost',
+        ip: '127.0.0.1',
+        port: process.env.PORT || 5000,
+        imagePort: ':' + (process.env.PORT || 5000),
+    };
+}
